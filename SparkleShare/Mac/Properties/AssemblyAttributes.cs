f5c14471f2feb1f3aa7c8e8aa16a83dd78250ea9
Markus Stoll <markus.stoll@junidas.de>
2026-05-20 19:16:50 +0200

    Bump macOS release version to 4.0.1-beta.
    Align csproj, Info.plist, assembly metadata, and meson VERSION for the next distribution build.

--- SparkleShare/Mac/Properties/AssemblyAttributes.cs
+++ SparkleShare/Mac/Properties/AssemblyAttributes.cs
@@ -3,6 +3,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion ("4.0.0.0")]
-[assembly: AssemblyFileVersion ("4.0.0.0")]
-[assembly: AssemblyInformationalVersion ("4.00-beta")]
+[assembly: AssemblyVersion ("4.0.1.0")]
+[assembly: AssemblyFileVersion ("4.0.1.0")]
+[assembly: AssemblyInformationalVersion ("4.0.1-beta")]
