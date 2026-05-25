997d4d2e4c0ffef9f4bc42861941026c8bdcdb0c
Markus Stoll <markus.stoll@junidas.de>
2026-05-25 22:21:17 +0200

    Release 4.0.3
    Bump version across Mac/Windows installers and release notes.

--- SparkleShare/Mac/Properties/AssemblyAttributes.cs
+++ SparkleShare/Mac/Properties/AssemblyAttributes.cs
@@ -3,6 +3,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion ("4.0.2.0")]
-[assembly: AssemblyFileVersion ("4.0.2.0")]
-[assembly: AssemblyInformationalVersion ("4.0.2")]
+[assembly: AssemblyVersion ("4.0.3.0")]
+[assembly: AssemblyFileVersion ("4.0.3.0")]
+[assembly: AssemblyInformationalVersion ("4.0.3")]
--- SparkleShare/Windows/Properties/AssemblyAttributes.cs
+++ SparkleShare/Windows/Properties/AssemblyAttributes.cs
@@ -2,6 +2,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion ("4.0.2.0")]
-[assembly: AssemblyFileVersion ("4.0.2.0")]
-[assembly: AssemblyInformationalVersion ("4.0.2")]
+[assembly: AssemblyVersion ("4.0.3.0")]
+[assembly: AssemblyFileVersion ("4.0.3.0")]
+[assembly: AssemblyInformationalVersion ("4.0.3")]
